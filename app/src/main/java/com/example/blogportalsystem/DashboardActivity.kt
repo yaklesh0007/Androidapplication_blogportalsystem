9d57d58618b97e9d15f64ffcb627502243edac54
aklesh <rockeroflife609@gmail.com>
2021-01-22 23:09:57 +0545

    make changes and add content_main file for Action bar

--- app/src/main/java/com/example/blogportalsystem/DashboardActivity.kt
+++ app/src/main/java/com/example/blogportalsystem/DashboardActivity.kt
@@ -35,7 +35,7 @@ class DashboardActivity : AppCompatActivity(),NavigationView.OnNavigationItemSel
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
        when(item.itemId){
             R.id.nav_home->{
-                Toast.makeText(this, "Home navigation has been clicked", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@Dashboard, "Home navigation has been clicked", Toast.LENGTH_SHORT).show()
             }
            R.id.nav_profile->{
                Toast.makeText(this, "Home navigation has been clicked", Toast.LENGTH_SHORT).show()
