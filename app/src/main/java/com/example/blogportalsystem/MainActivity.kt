ba1cb0f1bb89c9f6fe5445ad25e26fcec404d856
aklesh <rockeroflife609@gmail.com>
2021-01-29 22:14:42 +0545

    add validation in login form

--- app/src/main/java/com/example/blogportalsystem/MainActivity.kt
+++ app/src/main/java/com/example/blogportalsystem/MainActivity.kt
@@ -17,24 +17,24 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var edtemail:EditText
-    private lateinit var edtpassword:EditText
-    private lateinit var btnlogin:Button
-    private lateinit var tvRegister:TextView
+    private lateinit var edtemail: EditText
+    private lateinit var edtpassword: EditText
+    private lateinit var btnlogin: Button
+    private lateinit var tvRegister: TextView
 
-    var lstUsers= arrayListOf<User>()
+    var lstUsers = arrayListOf<User>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        edtemail=findViewById(R.id.edtemail)
-        edtpassword=findViewById(R.id.edtpassword)
-        btnlogin=findViewById(R.id.btnlogin)
+        edtemail = findViewById(R.id.edtemail)
+        edtpassword = findViewById(R.id.edtpassword)
+        btnlogin = findViewById(R.id.btnlogin)
 
-        tvRegister=findViewById(R.id.tvRegister)
+        tvRegister = findViewById(R.id.tvRegister)
 
-        lstUsers= arrayListOf<User>()
+        lstUsers = arrayListOf<User>()
         tvRegister.setOnClickListener {
-                startActivity(Intent(this@MainActivity,SignUpActivity::class.java))
+            startActivity(Intent(this@MainActivity, SignUpActivity::class.java))
         }
 
         btnlogin.setOnClickListener {
@@ -44,24 +44,39 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     private fun login() {
-        val email = edtemail.text.toString()
-        val password = edtpassword.text.toString()
-        var user: User? = null
-        CoroutineScope(Dispatchers.IO).launch {
-            user = UserDB
-                .getInstance(this@MainActivity)
-                .getUserDao()
-                .checkUser(email,password)
-            if(user == null){
-                withContext(Main){
-                    Toast.makeText(this@MainActivity, "Invalid credentials", Toast.LENGTH_SHORT)
-                        .show()
+
+        if (edtemail.text.isEmpty()) {
+            edtemail.error = "Email must not be empty!!"
+        } else if (edtpassword.text.isEmpty()) {
+            edtpassword.error = "Password must not be empty!!"
+        } else {
+            val email = edtemail.text.toString()
+            val password = edtpassword.text.toString()
+
+            var user: User? = null
+            CoroutineScope(Dispatchers.IO).launch {
+                user = UserDB
+                    .getInstance(this@MainActivity)
+                    .getUserDao()
+                    .checkUser(email, password)
+                if (user == null) {
+                    withContext(Main) {
+                        Toast.makeText(this@MainActivity, "Invalid credentials", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                } else {
+                    startActivity(
+                        Intent(
+                            this@MainActivity,
+                            DashboardActivity::class.java
+                        )
+                    )
                 }
-            }else{
-                startActivity(Intent(this@MainActivity,
-                    DashboardActivity::class.java))
+
             }
+
         }
     }
 }
