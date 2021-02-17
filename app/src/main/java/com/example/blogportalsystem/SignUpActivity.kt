3507e915c26c8b9f046e455aeb29be93f07c0b48
aklesh <yaklesh0007@gmail.com>
2021-02-17 15:20:44 +0545

    add dependency to the build gridle file and added service builder object

--- app/src/main/java/com/example/blogportalsystem/SignUpActivity.kt
+++ app/src/main/java/com/example/blogportalsystem/SignUpActivity.kt
@@ -4,9 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import com.example.blogportalsystem.db.UserDB
 import com.example.blogportalsystem.model.User
 import kotlinx.coroutines.CoroutineScope
@@ -20,9 +18,13 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var edtemail: EditText
     private lateinit var edtpassword: EditText
     private lateinit var edtphone: EditText
-    private lateinit var edtimage: EditText
+    private lateinit var RdoGroup:RadioGroup
+    private lateinit var RdoMale:RadioButton
+    private lateinit var RdoFemale:RadioButton
+    private lateinit var RdoOther:RadioButton
     private lateinit var btnSignup: Button
-
+    private var userType:String="normaluser"
+    var gender:String=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -30,7 +32,10 @@ class SignUpActivity : AppCompatActivity() {
         edtemail = findViewById(R.id.edtemail)
         edtpassword = findViewById(R.id.edtpassword)
         edtphone = findViewById(R.id.edtphone)
-        edtimage = findViewById(R.id.edtimage)
+        RdoGroup=findViewById(R.id.RdoGroup)
+        RdoMale=findViewById(R.id.RdoMale)
+        RdoFemale=findViewById(R.id.RdoFemale)
+        RdoOther=findViewById(R.id.RdoOther)
         btnSignup = findViewById(R.id.btnSignup)
 
         btnSignup.setOnClickListener {
@@ -42,39 +47,53 @@ class SignUpActivity : AppCompatActivity() {
                 edtpassword.error = "passord is required !!"
             } else if (edtphone.text.isEmpty()) {
                 edtphone.error = "Phone number is required !!"
-            } else if (edtimage.text.isEmpty()) {
-                edtimage.error = "Image is required !!"
             } else {
                 var fullname = edtfullname.text.toString()
                 var phone = edtphone.text.toString()
                 var email = edtemail.text.toString()
                 var password = edtpassword.text.toString()
-                var image = edtimage.text.toString()
-
-                val user = User(fullname, email, password, phone, image)
-
-                CoroutineScope(Dispatchers.IO).launch {
-                    UserDB
-                        .getInstance(this@SignUpActivity)
-                        .getUserDao()
-                        .registerUser(user)
-                    withContext(Main) {
-                        Toast.makeText(
-                            this@SignUpActivity,
-                            "User register Successfully",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+                when{
+                    RdoMale.isChecked->{
+                        gender="Male"
+                    }
+                    RdoFemale.isChecked->{
+                        gender="Female"
+                    }
+                    RdoOther.isChecked->{
+                        gender="Other"
                     }
                 }
-                edtfullname.text.clear()
-                edtphone.text.clear()
-                edtemail.text.clear()
-                edtpassword.text.clear()
-                edtimage.text.clear()
+
+                val user = User(fullname, email, password, phone)
+
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    UserDB
+//                        .getInstance(this@SignUpActivity)
+//                        .getUserDao()
+//                        .registerUser(user)
+//                    withContext(Main) {
+//                        Toast.makeText(
+//                            this@SignUpActivity,
+//                            "User register Successfully",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                        startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+//                    }
+//                }
+                reset()
+
             }
 
         }
+
+    }
+    private  fun reset(){
+        edtfullname.text.clear()
+        edtphone.text.clear()
+        edtemail.text.clear()
+        edtpassword.text.clear()
+    }
+    private fun SaveUser(){
 
     }
 }
