c3a42a7a42266e1ea1bfe27f0a99e8a21786d59e
aklesh <yaklesh0007@gmail.com>
2021-04-13 12:13:18 +0545

    added design for more page

--- blogportalwear/src/main/java/com/example/blogportalwear/WearableDashboardActivity.kt
+++ blogportalwear/src/main/java/com/example/blogportalwear/WearableDashboardActivity.kt
@@ -2,12 +2,24 @@ package com.example.blogportalwear
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class WearableDashboardActivity : WearableActivity() {
-
+private lateinit var Imgwearprofile:ImageView
+private lateinit var TvwearUsername:TextView
+private lateinit var TvwearEmail:TextView
+private lateinit var TvwearGender:TextView
+private lateinit var BtnwearNotification:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wearable_dashboard)
+        Imgwearprofile=findViewById(R.id.Imgwearprofile)
+        TvwearUsername=findViewById(R.id.TvwearUsername)
+        TvwearEmail=findViewById(R.id.TvwearEmail)
+        TvwearGender=findViewById(R.id.TvwearGender)
+        BtnwearNotification=findViewById(R.id.BtnwearNotification)
 
         // Enables Always-on
         setAmbientEnabled()
