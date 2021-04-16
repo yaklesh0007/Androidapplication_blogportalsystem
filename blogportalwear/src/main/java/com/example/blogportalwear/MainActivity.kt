5a78fae2984e9c5fa9b021e76422830edbd591a1
aklesh <yaklesh0007@gmail.com>
2021-04-16 16:18:22 +0545

    added login in Wearable

--- blogportalwear/src/main/java/com/example/blogportalwear/MainActivity.kt
+++ blogportalwear/src/main/java/com/example/blogportalwear/MainActivity.kt
@@ -3,9 +3,17 @@ package com.example.blogportalwear
 import android.content.Intent
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.blogportalwear.APIFORWEAR.ServiceBuilder
+import com.example.blogportalwear.RepositoryForwear.UserRepository
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : WearableActivity() {
  private lateinit var Etemail:EditText
@@ -18,13 +26,43 @@ class MainActivity : WearableActivity() {
         Etpassword=findViewById(R.id.Etpassword)
         Btnlogin=findViewById(R.id.Btnlogin)
         Btnlogin.setOnClickListener {
-            var email=Etemail.text.toString()
-            var password=Etpassword.text.toString()
-            Toast.makeText(this@MainActivity, "$email,$password", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this@MainActivity,WearableDashboardActivity::class.java))
+            login()
         }
 
         // Enables Always-on
         setAmbientEnabled()
+    }
+    private fun login() {
+        var email=Etemail.text.toString()
+        var password=Etpassword.text.toString()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val repository = UserRepository()
+                val response = repository.checkUser(email, password)
+                if (response.success == true) {
+                    ServiceBuilder.token = "Bearer " + response.token
+                    startActivity(
+                        Intent(
+                            this@MainActivity,
+                            WearableDashboardActivity::class.java
+                        )
+                    )
+                    finish()
+
+                } else {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, "Invalid Creadential", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            } catch (ex: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "$ex", Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
