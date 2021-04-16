51ad997cb5095a869a8fcd370253d5cb2b0141c7
aklesh <yaklesh0007@gmail.com>
2021-04-16 20:06:00 +0545

    added show profile in watch

--- app/src/main/java/com/example/blogportalsystem/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/blogportalsystem/fragments/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,7 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.blogportalsystem.R
 import com.example.blogportalsystem.adapter.HomeAdapter
 import com.example.blogportalsystem.repository.PostRepository
@@ -26,7 +28,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var recyclerView:RecyclerView
     private lateinit var FabBtnAdd: FloatingActionButton
-
+//    private lateinit var Swiperefersh:SwipeRefreshLayout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,42 +37,48 @@ class HomeFragment : Fragment() {
        val view=  inflater.inflate(R.layout.fragment_home, container, false)
         recyclerView=view.findViewById(R.id.recyclerView)
         FabBtnAdd=view.findViewById(R.id.FabBtnAdd)
-
+//        Swiperefersh=view.findViewById(R.id.Swiperefersh)
         val adapter=HomeAdapter(context!!, emptyList())
         recyclerView.layoutManager=LinearLayoutManager(requireContext())
         recyclerView.adapter=adapter
-
+        getallblog()
         FabBtnAdd.setOnClickListener {
             startActivity(Intent(activity,AddPostActivity::class.java))
         }
-        if (checkNetwork(context!!)) {
-            getallblog()
-        }
-        else if (!checkNetwork(context!!)) {
+
+//        if (checkNetwork(context!!)) {
+//            Toast.makeText(context, "danadajdj", Toast.LENGTH_SHORT).show()
+//
+//        }
+        if (!checkNetwork(context!!)) {
             getdatafromroom()
+            Toast.makeText(context, "jjjjj", Toast.LENGTH_SHORT).show()
         }
+
+//        Swiperefersh.setOnRefreshListener {
+//
+//            Handler().postDelayed(Runnable {
+//                Swiperefersh.isRefreshing = false
+//            }, 4000)
+//        }
         return view
     }
 
     private fun getallblog(){
         CoroutineScope(Dispatchers.IO).launch {
-//          val  lsblog = BlogDB
-//                .getInstance(context!!)
-//                .getPostDAO()
-//                .getPost()
-//
-//            val adapter=HomeAdapter(context!!,lsblog)
-//            recyclerView.adapter=adapter
+
             try
             {
                 val blogrepository = PostRepository()
                 val response = blogrepository.getallBlog()
+
                 BlogDB
                     .getInstance(context!!)
                     .getPostDAO()
                     .insertPost(response.data!!)
                 if (response.success == true){
                     withContext(Dispatchers.Main){
+//                        Toast.makeText(context, "${response.data}", Toast.LENGTH_SHORT).show()
                         recyclerView.adapter = HomeAdapter(context!!,response.data!!)
                         recyclerView.layoutManager= LinearLayoutManager(context)
                     }
@@ -85,7 +93,6 @@ class HomeFragment : Fragment() {
     }
     private fun checkNetwork(context: Context): Boolean {
         val connManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
 
         return true
     }
--- blogportalwear/src/main/java/com/example/blogportalwear/WearableDashboardActivity.kt
+++ blogportalwear/src/main/java/com/example/blogportalwear/WearableDashboardActivity.kt
@@ -5,6 +5,14 @@ import android.support.wearable.activity.WearableActivity
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.example.blogportalwear.APIFORWEAR.ServiceBuilder
+import com.example.blogportalwear.RepositoryForwear.UserRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class WearableDashboardActivity : WearableActivity() {
 private lateinit var Imgwearprofile:ImageView
@@ -20,11 +28,44 @@ private lateinit var BtnwearNotification:Button
         TvwearEmail=findViewById(R.id.TvwearEmail)
         TvwearGender=findViewById(R.id.TvwearGender)
         BtnwearNotification=findViewById(R.id.BtnwearNotification)
-
+        loaduser()
         // Enables Always-on
         setAmbientEnabled()
     }
     private fun loaduser(){
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val userRepository= UserRepository()
+                val response=userRepository.showprofile()
+                if (response.success==true){
+                    val userData=response.data
+
+                    withContext(Dispatchers.Main){
+
+                        TvwearUsername.text="Username: "+userData!!.username
+                        TvwearEmail.text="Email: "+userData!!.email
+
+                        TvwearGender.text="Gender: "+userData!!.gender
+
+                        userData.image?.let {
+
+                            Glide.with(this@WearableDashboardActivity!!)
+                                .load(ServiceBuilder.loadImagePath() + it)
+                                .into(Imgwearprofile)
+                        }
+                        Toast.makeText(this@WearableDashboardActivity, "welcome to ${userData.username}", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+                else{
+
+                    Toast.makeText(this@WearableDashboardActivity, "your information was not found", Toast.LENGTH_SHORT).show()
+                }
+            }
+            catch (ex:Exception){
+                print(ex)
+            }
+        }
 
     }
 }
